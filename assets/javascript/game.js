6e7a0374effbe137e7c6fc7147ea64f42e871694
WagnerAndy <wagner.andrew.james@gmail.com>
2018-11-11 20:48:50 -0700

    close to final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,14 +4,26 @@ var words = ['banana', 'apple', 'orange', 'grape', 'pear', 'kiwi', 'plum'];
 var word = words[Math.floor(Math.random() * words.length)];
 
 var wordArray = word.split("");
+var incorrectGuessesArray = [];
+var answerArray = [];
+
 var wordText = document.getElementById("wordText");
 var wordGuessesText = document.getElementById("guessCount");
-
 var lettersGuessedText = document.getElementById("lettersGuessed");
-var remainingGuesses = 10;
-var incorrectGuessesArray = [];
+var winCountText = document.getElementById("winCount");
+var lossCountText = document.getElementById("lossCount");
 
-var answerArray = [];
+var remainingGuesses = 10;
+var winCount = 0;
+var lossCount = 0;
+
+
+function randomWord() {
+    var randomWordGenerator = letters[Math.floor(Math.random() * letters.length)];
+
+    return randomWordGenerator;
+};
+
 
 for (var i = 0; i < word.length; i++) {
     answerArray[i] = "_ ";
@@ -23,9 +35,8 @@ var remainingLetters = word.length;
 wordText.textContent = (answerArray.join(""));
 
 console.log("word is: " + word);
+console.log("wordArray is: " + wordArray);
 console.log("answerArray is: " + answerArray);
-
-
 
 document.onkeyup = function (event) {
 
@@ -36,42 +47,55 @@ document.onkeyup = function (event) {
     if (letters.indexOf(guess) > -1) {
         console.log("guess is: " + guess);
 
-        if (incorrectGuessesArray.indexOf(guess) > -1 || answerArray.indexOf(guess) > -1){ 
+        if (incorrectGuessesArray.indexOf(guess) > -1 || answerArray.indexOf(guess) > -1) {
             return alert("You already guessed that letter!");
-          };
+        };
 
 
         if (wordArray.indexOf(guess) > -1) {
             for (var j = 0; j < wordArray.length; j++) {
                 if (word[j] === guess) {
                     answerArray[j] = guess;
-
-                } 
+                    remainingGuesses--;
+                }
             }
-
+            
         } else {
             remainingGuesses--;
             incorrectGuessesArray.push(guess);
         }
 
-        
-        wordText.textContent = answerArray.join (" ");
+        wordText.textContent = answerArray.join(" ");
         wordGuessesText.textContent = remainingGuesses
         lettersGuessedText.textContent = incorrectGuessesArray;
 
+        console.log("answerArray after guess is: " + answerArray);
+        console.log("wordArray after guess is: " + wordArray);
 
+        if (wordArray === answerArray) {
+            winCount++;
+            winCountText.textContent = winCount;
+            wordArray = [];
+            remainingGuesses = 10;
+            wordGuessesText.textContent = remainingGuesses;
+            incorrectGuessesArray = [];
+            lettersGuessedText.textContent = incorrectGuessesArray;
+            } else {
 
+            
 
-
-
-
-
+        if (remainingGuesses === 0) {
+            lossCount++;
+            lossCountText.textContent = lossCount;
+            remainingGuesses = 10;
+            wordGuessesText.textContent = remainingGuesses;
+            incorrectGuessesArray = [];
+            }
+        }
 
 
     }
-
-
-
+    
     // if (letters.indexOf(guess) !== -1){
 
     //     for (var j = 0; j < word.length; j++){
